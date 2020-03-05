7bd67cd59d14266a2c9b1979b4874c14273e8e1d
Shota Kashihara <mercury4th@gmail.com>
2020-03-05 12:01:21 +0900

    update

---
+++ DiffarableSamples/SampleViewController.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+private extension SampleViewController {
+    enum RowType: Int, CaseIterable {
+        case textStyle
+        case pageHeader
+    }
+}
+
+final class SampleViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    private func setupView() {
+        title = "Diffarable"
+
+        navigationController?.navigationBar.titleTextAttributes = [
+            .font: UIFont.systemFont(ofSize: 17)
+        ]
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
+        tableView.tableFooterView = UIView()
+        tableView.separatorInset = .zero
+        tableView.rowHeight = 60.0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return RowType.allCases.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: UITableViewCell.self), for: indexPath)
+        cell.textLabel?.text = RowType(rawValue: indexPath.row)!.title
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 17)
+        cell.textLabel?.textAlignment = .center
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let viewController = RowType(rawValue: indexPath.row)!.viewController
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
+extension SampleViewController.RowType {
+    var title: String {
+        switch self {
+        case .textStyle:
+            return "TextStyle"
+        case .pageHeader:
+            return "Page Header"
+        }
+    }
+
+    var viewController: UIViewController {
+        switch self {
+        case .textStyle:
+            return UIViewController()
+        case .pageHeader:
+            return UIViewController()
+        }
+    }
+}
